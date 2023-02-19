528125e5f4fb4f83b85ab493aed2631c8605e2dd
unknown <aayushprajapati867gmail.com>
2023-02-20 00:01:11 +0530

    Day-19 code

---
+++ 2ND 15/code19_2.c
@@ -0,0 +1,33 @@
+/*
+     *
+    * *
+   * * *
+  * * * *
+ * * * * *
+*/
+#include<stdio.h>
+int main()
+{
+    int n=5,i,j,k;
+    // printf("Enter the number of rows\n");
+    // scanf("%d",&n);
+    for (i=1;i<=n;i++)
+    {
+        for (j=n-1;j>=i;j--)
+        {
+            printf(" ");
+        }
+        for (k=1;k<=i;k++)
+        {
+        printf("*");
+        }
+        
+        
+        printf("\n");
+        
+    }
+    
+
+
+    return 0;
+}
